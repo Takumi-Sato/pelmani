fd1a6a46efa927ff1fb14b027d0e47560dba6163
Takumi-Sato <s1511378@coins.tsukuba.ac.jp>
2018-01-30 17:39:15 +0000

    spi test, multiple I/O Expanders availablegit status Input to I/O Expanders AVAILABLEgit status

--- spi/input_test.c
+++ spi/input_test.c
@@ -9,6 +9,8 @@
 #define IOCON 0x0a
 #define GPIOA 0x12
 #define GPIOB 0x13
+#define GPPUA 0x0c
+#define GPPUB 0x0d
 #define DEVICE_ADDR 0x40
 #define DEVICE_0 0x40
 #define DEVICE_1 0x42
@@ -16,9 +18,12 @@
 #define CLOCK 10000000
 
 void write2spi(unsigned char device, unsigned char regaddr, unsigned char tx_data);
+void read_from_spi(unsigned char device, unsigned char resgaddr, unsigned char* res);
 
 int main(void)
 {
+  unsigned char res[3];
+
   if((wiringPiSPISetup (SPI_CHANNEL, CLOCK)) < 0) {
     printf("wiringPiSPISetup error \n");
     return -1;
@@ -32,12 +37,13 @@ int main(void)
   digitalWrite(SS_PORT, 1);
 
   write2spi(DEVICE_ADDR, IOCON, 0x08);
+  write2spi(DEVICE_1, GPPUB, 0xff);
   write2spi(DEVICE_0, IODIRA, 0x00);
-  write2spi(DEVICE_1, IODIRB, 0x00);
-  write2spi(DEVICE_0, GPIOA, 0x00);
-  write2spi(DEVICE_1, GPIOB, 0x00);
-  write2spi(DEVICE_0, GPIOA, 0xff);
-  write2spi(DEVICE_1, GPIOB, 0xff);
+  write2spi(DEVICE_1, IODIRB, 0xff);
+  write2spi(DEVICE_0, GPIOA, 0x40);
+  read_from_spi(DEVICE_1, GPIOB, res);
+  printf("res = 0x%x, 0x%x, 0x%x\n", res[0], res[1], res[2]);
+ // write2spi(DEVICE_0, GPIOA, 0xff);
 
   return 0;
 }
@@ -57,4 +63,18 @@ void write2spi(unsigned char device, unsigned char regaddr, unsigned char tx_dat
   printf("[AFTER]\ndevice:0x%x, regaddr:0x%x, data:0x%x\n", buf[0], buf[1], buf[2]);
 }
 
+void read_from_spi(unsigned char device, unsigned char regaddr, unsigned char* res)
+{
+  device = device | 0x01; // read from spi device
+  res[0] = device;
+  res[1] = regaddr;
+  res[2] = 0x0;
+  res[3] = 0x0;
 
+  printf("[BEFORE]\ndevice:0x%x, regaddr:0x%x\n", res[0], res[1]);
+  digitalWrite(SS_PORT, 0);
+  wiringPiSPIDataRW(SPI_CHANNEL, res, 3);
+  digitalWrite(SS_PORT, 1);
+
+  printf("[AFTER]\nbuf[0]:0x%x, buf[1]:0x%x, buf[2]:0x%x, buf[3]:0x%x\n", res[0], res[1], res[2], res[3]);
+}
